5d16be07857e93a9fed9a4f68ef1d461a367343c
Sean Lovinger <smlovin2@users.noreply.github.com>
2018-07-10 22:59:18 -0700

    refactor: Clean up more eslint errors
    Define functions, add semi-colons, etc.

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -8,7 +8,7 @@ class DBHelper {
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-    const port = 8000 // Change this to your server port
+    const port = 8000; // Change this to your server port
     return `http://localhost:${port}/data/restaurants.json`;
   }
 
@@ -91,7 +91,7 @@ class DBHelper {
       if (error) {
         callback(error, null);
       } else {
-        let results = restaurants
+        let results = restaurants;
         if (cuisine != 'all') { // filter by cuisine
           results = results.filter(r => r.cuisine_type == cuisine);
         }
@@ -113,9 +113,9 @@ class DBHelper {
         callback(error, null);
       } else {
         // Get all neighborhoods from all restaurants
-        const neighborhoods = restaurants.map((v, i) => restaurants[i].neighborhood)
+        const neighborhoods = restaurants.map((v, i) => restaurants[i].neighborhood);
         // Remove duplicates from neighborhoods
-        const uniqueNeighborhoods = neighborhoods.filter((v, i) => neighborhoods.indexOf(v) == i)
+        const uniqueNeighborhoods = neighborhoods.filter((v, i) => neighborhoods.indexOf(v) == i);
         callback(null, uniqueNeighborhoods);
       }
     });
@@ -131,9 +131,9 @@ class DBHelper {
         callback(error, null);
       } else {
         // Get all cuisines from all restaurants
-        const cuisines = restaurants.map((v, i) => restaurants[i].cuisine_type)
+        const cuisines = restaurants.map((v, i) => restaurants[i].cuisine_type);
         // Remove duplicates from cuisines
-        const uniqueCuisines = cuisines.filter((v, i) => cuisines.indexOf(v) == i)
+        const uniqueCuisines = cuisines.filter((v, i) => cuisines.indexOf(v) == i);
         callback(null, uniqueCuisines);
       }
     });
@@ -154,30 +154,30 @@ class DBHelper {
     const imgName = restaurant.photograph.url.substr(0, startOfFileEnding);
     const imgType = restaurant.photograph.url.substr(startOfFileEnding, restaurant.photograph.length);
     switch(size) {
-      case 'small':
-        return (`/img/${imgName}-small${imgType}`);
-      case 'medium':
-        return (`/img/${imgName}-medium${imgType}`);
-      case 'large1x':
-        return (`/img/${imgName}-800_large_1x${imgType}`);
-      case 'large2x':
-        return (`/img/${imgName}-1600_large_2x${imgType}`);
-      default:
-        return (`/img/${imgName}-small${imgType}`);
+    case 'small':
+      return (`/img/${imgName}-small${imgType}`);
+    case 'medium':
+      return (`/img/${imgName}-medium${imgType}`);
+    case 'large1x':
+      return (`/img/${imgName}-800_large_1x${imgType}`);
+    case 'large2x':
+      return (`/img/${imgName}-1600_large_2x${imgType}`);
+    default:
+      return (`/img/${imgName}-small${imgType}`);
     }
   }
 
   /**
    * Map marker for a restaurant.
    */
-   static mapMarkerForRestaurant(restaurant) {
+  static mapMarkerForRestaurant(restaurant) {
     // https://leafletjs.com/reference-1.3.0.html#marker  
     const marker = new L.marker([restaurant.latlng.lat, restaurant.latlng.lng],
       {title: restaurant.name,
-      alt: restaurant.name,
-      url: DBHelper.urlForRestaurant(restaurant)
-      })
-      marker.addTo(newMap);
+        alt: restaurant.name,
+        url: DBHelper.urlForRestaurant(restaurant)
+      });
+    marker.addTo(newMap);
     return marker;
   } 
   /*
@@ -194,4 +194,3 @@ class DBHelper {
    */
 
 }
-
